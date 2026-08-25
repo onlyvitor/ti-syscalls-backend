d2b836be4320f93b470d22ec69995c30af59dd1f
Vitor Gabriel Nascimento França <vitorgn1franca@protonmail.com>
2026-08-24 22:58:04 -0300

    feat: added in memory searchable user repository

---
+++ src/users/infrastructure/repositories/in-memory-searchable-user.repository.ts
@@ -0,0 +1,13 @@
+import { SearchableUserRepository } from 'src/users/domain/repositories/searchable-user.repository';
+import { User } from '../../domain/entities/user.entity';
+import { InMemoryUserRepository } from './in-memory-user.repository';
+
+export class InMemorySearchableUserRepository
+  extends InMemoryUserRepository
+  implements SearchableUserRepository<User, any, User[]>
+{
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  search(_props: any): Promise<User[]> {
+    throw new Error('Method not implemented.');
+  }
+}
